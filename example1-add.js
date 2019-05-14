6609406c49889ab8a46c3dfdd549856049777598
Jesús Efrén López Salado <39703278+efrensalado@users.noreply.github.com>
2019-05-14 10:30:44 -0500

    Create example1-add.js

---
+++ example1-add.js
@@ -0,0 +1,18 @@
+function add(param1, param2) {
+    res = param1 + param2;
+    //console.log(res);
+    return res;
+}
+
+//test1
+console.log(add(1, 2));
+//test2
+console.log(add(0, 1000));
+//test3
+console.log(add(2, -39));
+//test4
+console.log(add(99, 100));
+//test5
+console.log(add(-100, 100));
+//test6
+console.log(add(-1000, -1000));
